6f7c3d7bd3e9cae1aa08b2bca2ab9f545ca30c5c
darrenak403 <ngothanhdat4002@gmail.com>
2026-06-01 18:31:11 +0700

    feat(i18n): update vietnamese translations for enrollment and call logs
    Changes:
    - Add translation for "Enrollment Students" and "Enrollment Student"
    - Add translation for "Call Logs" and "Call Log"

--- frontend/src/components/Layouts/AppSidebar.vue
+++ frontend/src/components/Layouts/AppSidebar.vue
@@ -261,7 +261,7 @@ const links = [
     to: 'Call Logs',
   },
   {
-    label: 'Học sinh nhập học',
+    label: 'Enrollment Students',
     icon: GraduationCapIcon,
     to: 'Enrollment Students',
   },
